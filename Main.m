1441fe05253e3ecc81bc444fa0fde5a4fcee991f
linzhiheng <linx-x@live.com>
2016-06-12 22:32:32 +0800

    确定初始模型

--- Main.m
+++ Main.m
@@ -2,17 +2,18 @@
 close all
 clear all
 clc
+
 % known constant
 gravity = 9.8;
-Depth = 100;
+Depth = 1000;
 amplitude = 1;
-angularFrequency = 0.6;
+angularFrequency = 0.2;
 
 permeability = 1.2566*10^-6;
 waterConductivity = 5;
 floorConductivity = 1;
 
-earthField = 0.5;
+earthField = 5*10^-5;
 dipEarthField = 70;
 inclinationEarthField = 0;
 Fx = earthField*cos(dipEarthField)*cos(inclinationEarthField);
@@ -32,5 +33,11 @@ hz1 = @(z) C(1)*exp(k*z);
 hz2 = @(z) C(2)*exp(B1*z) + C(3)*exp(-B1*z) + A*( i*sinh(k*(Depth-z))*Fx + cosh(k*(Depth-z))*Fz);
 hz3 = @(z) C(4)*exp(-B2*z);
 
+% Get horizontal magnetic form
+A = (amplitude*gravity/angularFrequency)/cosh(k*Depth);
+hx1 = @(z) -i*C(1)*exp(k*z);
+hx2 = @(z) (-i/k)*B1*C(2)*exp(B1*z) + (i/k)*B1*C(3)*exp(-B1*z) + A*( -cosh(k*(Depth-z))*Fx + i*sinh(k*(Depth-z))*Fz);
+hx3 = @(z) (i/k)*B2*C(4)*exp(-B2*z);
+
 % Plot hz
-Plot_hz(hz1,hz2,hz3,Depth)
+Plot_h(hz1,hz2,hz3,Depth)
--- Plot_h.m
+++ Plot_h.m
@@ -1,13 +1,13 @@
 % Plot h
 function Plot_h(hz1,hz2,hz3,D)
-z = -5*D:1:0;
+z = -3*D:1:0;
 plot(abs(hz1(z)),z)
 set(gca,'YDir','reverse')
 hold on
 z = 0:1:D;
 plot(abs(hz2(z)),z)
 hold on
-z = D:1:10*D;
+z = D:1:5*D;
 plot(abs(hz3(z)),z)
 grid on
 hold off
